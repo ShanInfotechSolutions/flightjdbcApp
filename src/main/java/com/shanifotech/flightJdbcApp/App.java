dd307a256733ea633bf6f602300eb9a6710748e0
ShanInfotechSolutions <shanmukh@shaninfotechsolutions.com>
2025-07-07 13:14:30 +0530

    Apppppp

--- src/main/java/com/shanifotech/flightJdbcApp/App.java
+++ src/main/java/com/shanifotech/flightJdbcApp/App.java
@@ -3,6 +3,10 @@ package com.shanifotech.flightJdbcApp;
 public class App {
     public static void main(String[] args) throws Exception {
         FlightDetailsDao fd=new FlightDetailsDao();
+        FlightDetails f=new FlightDetails();
+        
+        
+        fd.addFlight(f);
         fd.getFlight();
         
     }
