7a0ae523aaff2c67918be32ef382a669a0b4d4ca
ShanInfotechSolutions <shanmukh@shaninfotechsolutions.com>
2025-07-07 12:55:45 +0530

    App CXlass

--- src/main/java/com/shanifotech/flightJdbcApp/App.java
+++ src/main/java/com/shanifotech/flightJdbcApp/App.java
@@ -3,7 +3,7 @@ package com.shanifotech.flightJdbcApp;
 import java.sql.SQLException;
 
 public class App {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws Exception {
         FlightDetailsDao fd=new FlightDetailsDao();
         //fd.addFlight(new FlightDetails());
         fd.getFlight();
