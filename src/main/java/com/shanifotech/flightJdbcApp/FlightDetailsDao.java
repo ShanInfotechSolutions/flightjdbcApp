3aca00ca8508825d1561d499a6dd0b572adfc245
ShanInfotechSolutions <shanmukh@shaninfotechsolutions.com>
2025-07-07 13:06:07 +0530

    dao and interface

--- src/main/java/com/shanifotech/flightJdbcApp/FlightDetailsDao.java
+++ src/main/java/com/shanifotech/flightJdbcApp/FlightDetailsDao.java
@@ -14,7 +14,8 @@ public class FlightDetailsDao implements IFlightDetails{
 	
 
 	@Override
-	public void addFlight(FlightDetails fd) throws Exception {
+	public void addFlight(FlightDetails fd) throws ClassNotFoundException,SQLException {
+		try {
 		con=DBConnect.connect();
 		st=con.createStatement();
 		System.out.println("Flight Details: Id,FlightCompany,TravelDate,Source,Destination");
@@ -28,6 +29,10 @@ public class FlightDetailsDao implements IFlightDetails{
 				+fd.getTravelDate()+"','"+fd.getSource()+"','"+fd.getDestination()+"')";
 				st.executeUpdate(insertFlights);
 				System.out.println("Data Inserted");
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 	@Override
@@ -35,7 +40,8 @@ public class FlightDetailsDao implements IFlightDetails{
 		}
 
 	@Override
-	public void getFlight() throws Exception {
+	public void getFlight() throws ClassNotFoundException,SQLException {
+		try {
 		con=DBConnect.connect();
 		st=con.createStatement();
 		String queryResult="select * from flightdetails";// result set id used for fetching the data from the database and points from the 1st row
@@ -43,7 +49,10 @@ public class FlightDetailsDao implements IFlightDetails{
 		while(rs.next()) {
 			System.out.println("Flight Id: "+rs.getInt(1));// index number  based which uses digits, column based which uses column names
 		}
-		
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 	@Override
--- src/main/java/com/shanifotech/flightJdbcApp/IFlightDetails.java
+++ src/main/java/com/shanifotech/flightJdbcApp/IFlightDetails.java
@@ -1,8 +1,10 @@
 package com.shanifotech.flightJdbcApp;
 
+import java.sql.SQLException;
+
 public interface IFlightDetails {
-	public void addFlight(FlightDetails fd) throws Exception;
+	public void addFlight(FlightDetails fd) throws ClassNotFoundException,SQLException;
 	public void updateFlight();
-	public void getFlight() throws Exception;
+	public void getFlight() throws ClassNotFoundException,SQLException;
 	public void deleteFlight();
 }
