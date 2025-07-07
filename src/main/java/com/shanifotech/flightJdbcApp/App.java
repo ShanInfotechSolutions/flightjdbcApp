54adf5971fb899576aed4f0e41b0ea1a87bb5f60
ShanInfotechSolutions <shanmukh@shaninfotechsolutions.com>
2025-07-07 13:02:14 +0530

    Apppp Classss

--- src/main/java/com/shanifotech/flightJdbcApp/App.java
+++ src/main/java/com/shanifotech/flightJdbcApp/App.java
@@ -1,10 +1,9 @@
 package com.shanifotech.flightJdbcApp;
 
-import java.sql.SQLException;
-
 public class App {
     public static void main(String[] args) throws Exception {
         FlightDetailsDao fd=new FlightDetailsDao();
+        fd.getFlight();
         
     }
 }
